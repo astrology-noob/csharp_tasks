f848b60c8b33f6d0f2433b8621be5d1513f56f92
astrology-noob <0french0fries0@gmail.com>
2022-09-21 14:39:14 +0300

    кошачья практическая работа в процессе

---
+++ CatsPractice/CatsPractice/Cat.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Loader;
+using System.Drawing;
+
+namespace CatsPractice
+{
+    static class Loader
+    {
+        public static Cat GetKitten(bool hasTail, Cat.furColor furColor)
+        {
+            return new Cat(hasTail, furColor, 1000.0f);
+        }
+    }
+
+    internal class Cat : ICloneable
+    {
+        private static int catCount = 0;
+
+        public enum furColor { Black = 0, White = 1, Ginger = 2 }
+        
+        const int eyesCount = 2;
+        const int minWeight = 1000;
+        const int maxWeight = 10000;
+        public float weight;
+        private furColor CatFurColor;
+        private bool HasTail;
+
+        public Cat(bool tail, furColor fur)
+        {
+            CatFurColor = fur;
+            HasTail = tail;
+            catCount++;
+        }
+
+        public Cat(bool tail, furColor fur, float catWeight)
+        {
+            weight = catWeight;
+        }
+
+        public bool GetaHasTail()
+        {
+            return HasTail;
+        }
+
+        public furColor GetFurColor()
+        {
+            return CatFurColor;
+        }
+
+
+        private float eatenFood = 0;
+        public float GetEatenFood()
+        {
+            return eatenFood;
+        }
+
+        public bool IsAlive()
+        {
+            return weight >= minWeight & weight <= maxWeight? true : false;
+        }
+
+        public void Meow()
+        {
+            if (IsAlive())
+                weight -= 10;
+            else Console.WriteLine("Cat is dead");
+        }
+
+        public void Feed(float food)
+        {
+            if (IsAlive())
+            {
+                eatenFood += food;
+                weight += food;
+            }
+            else Console.WriteLine("Cat is dead");
+        }
+
+        public void Pee()
+        {
+            if (IsAlive())
+                weight -= 20;
+            else Console.WriteLine("Cat is dead");
+        }
+
+        public static int GetCatCount()
+        {
+            return catCount;
+        }
+
+        public object Clone()
+        {
+            return new Cat(HasTail, CatFurColor, weight);
+        }
+    }
+}
---
+++ CatsPractice/CatsPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using CatsPractice;
+
+Cat cat1 = Loader.GetKitten(true, Cat.furColor.Black);
+Cat cat2 = Loader.GetKitten(false, Cat.furColor.White);
+
+Console.WriteLine("Cat1 Feed to death");
+Console.WriteLine("Weight before: {0}", cat1.weight);
+
+while (cat1.IsAlive())
+{
+    cat1.Feed(100);
+}
+
+Console.WriteLine("Weight after: {0}", cat1.weight);
+Console.WriteLine("Eaten Food: {0}", cat1.GetEatenFood());
+
+Console.WriteLine("Cat2 Meow to death");
+cat2.Feed(200);
+Console.WriteLine("Weight before: {0}", cat2.weight);
+
+while (cat2.IsAlive())
+{
+    cat2.Meow();
+}
+
+Console.WriteLine("Weight after: {0}", cat2.weight);
+cat2.Pee();
+
+Cat cat3 = (Cat)cat1.Clone();
+Console.WriteLine("Cat count: {0}", Cat.GetCatCount());
